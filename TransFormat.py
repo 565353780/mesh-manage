e1e60c6dc86173e593861910b4e03ee9cc22ad26
565353780 <565353780@qq.com>
2022-01-24 16:50:31 +0800

    add pcd ply file trans code

---
+++ TransFormat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import open3d as o3d
+
+input_pointcloud_file_path = "./masked_pc/home/home_DownSample_8_masked_cut.pcd"
+output_pointcloud_file_path = "./masked_pc/home/home_DownSample_8_masked_cut_normal.ply"
+
+pointcloud = o3d.io.read_point_cloud(input_pointcloud_file_path, print_progress=True)
+
+o3d.io.write_point_cloud(
+    output_pointcloud_file_path,
+    pointcloud,
+    write_ascii=True,
+    print_progress=True)
+
