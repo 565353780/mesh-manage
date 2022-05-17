77f16671ec0b88f53cd9fde2538143721f9d3ab6
565353780 <565353780@qq.com>
2022-05-17 22:21:18 +0800

    finish quickly create sample mesh algo

--- sample_mesh.py
+++ sample_mesh.py
@@ -3,17 +3,19 @@
 
 import open3d as o3d
 
-mesh_file_path = "/home/chli/masked_pcd/home/home.ply"
+mesh_file_basename = "f14"
+
+mesh_file_path = "/home/chli/masked_pcd/" + mesh_file_basename + "/" + mesh_file_basename + ".ply"
 
 print("start read file...")
 mesh = o3d.io.read_triangle_mesh(mesh_file_path)
 print("finish read file")
 
-o3d.visualization.draw_geometries([mesh])
-exit()
-
 sample_mesh = mesh.simplify_vertex_clustering(0.01)
 print("finish sample")
 
-o3d.io.write_triangle_mesh("/home/chli/masked_pcd/home/sample_home.ply", sample_mesh)
+o3d.io.write_triangle_mesh("/home/chli/masked_pcd/" + mesh_file_basename + "/" + mesh_file_basename + "_sample_0.01_mesh.ply",
+                           sample_mesh,
+                           write_ascii=True)
 print("finish save")
+
