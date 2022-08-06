eecf6946b28a98c500ee3a8aa8c261c4b17c0bae
chLi <565353780@qq.com>
2022-08-06 18:41:49 +0800

    simplify Channel class init

--- Data/channel_point.py
+++ Data/channel_point.py
@@ -13,32 +13,26 @@ class ChannelPoint(object):
         g = self.getChannelValue("g")
         b = self.getChannelValue("b")
         if r is None or g is None or b is None:
-            return False
+            return True
         rgb = r << 16 | g << 8 | b | 1<<24
         self.setChannelValue("rgb", rgb)
         return True
 
     def setChannelValue(self, channel_name, channel_value):
-        set_rgb = channel_name == "rgb"
         if len(self.channel_list) == 0:
-            new_channel = Channel()
-            new_channel.setName(channel_name)
-            new_channel.setValue(channel_value)
+            new_channel = Channel(channel_name, channel_value)
             self.channel_list.append(new_channel)
             return True
+
         for exist_channel in self.channel_list:
             if exist_channel.name == channel_name:
                 exist_channel.setValue(channel_value)
-                if channel_name in ["r", "g", "b"] and not set_rgb:
-                    self.updateFloatRGB()
+                self.updateFloatRGB()
                 return True
 
-        new_channel = Channel()
-        new_channel.setName(channel_name)
-        new_channel.setValue(channel_value)
+        new_channel = Channel(channel_name, channel_value)
         self.channel_list.append(new_channel)
-        if channel_name in ["r", "g", "b"] and not set_rgb:
-            self.updateFloatRGB()
+        self.updateFloatRGB()
         return True
 
     def setChannelValueList(self, channel_name_list, channel_value_list):
