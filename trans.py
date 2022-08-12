786517759620314e276852eabedd91c29507fbbf
chLi <565353780@qq.com>
2022-08-12 15:22:16 +0800

    update file path

--- trans.py
+++ trans.py
@@ -4,7 +4,8 @@
 from Method.trans_format import transToPLY
 
 # Param
-source_pointcloud_file_path = "./masked_pc/office/office_cut.pcd"
+source_pointcloud_file_path = \
+    "/home/chli/cad_ws/build/pcl_catkin/pcl_src-prefix/src/pcl_src/test/2d/canny.pcd"
 
 estimate_normals_radius = 0.05
 estimate_normals_max_nn = 30
