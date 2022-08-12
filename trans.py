3d197b7c7e5ea1bb2cbf8f546aafcb6ffd1c2216
chLi <565353780@qq.com>
2022-08-12 16:27:56 +0800

    simplify transFormat code

--- trans.py
+++ trans.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from Method.trans_format import transToPLY
+from Method.trans_format import transFormat
 
 # Param
 source_pointcloud_file_path = \
     "/home/chli/cad_ws/build/pcl_catkin/pcl_src-prefix/src/pcl_src/test/2d/canny.pcd"
-
+target_pointcloud_file_path = \
+    "/home/chli/cad_ws/build/pcl_catkin/pcl_src-prefix/src/pcl_src/test/2d/canny.ply"
+need_estimate_normals = True
 estimate_normals_radius = 0.05
 estimate_normals_max_nn = 30
 
 # Process
-transToPLY(source_pointcloud_file_path, estimate_normals_radius, estimate_normals_max_nn)
+transFormat(source_pointcloud_file_path, target_pointcloud_file_path,
+            True, estimate_normals_radius, estimate_normals_max_nn)
+
+transFormat(source_pointcloud_file_path, source_pointcloud_file_path,
+            True, estimate_normals_radius, estimate_normals_max_nn)
 
