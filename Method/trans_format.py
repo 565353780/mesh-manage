4c9b35911ac977af476d029b840e78a56c2d680d
chLi <565353780@qq.com>
2022-08-12 16:27:48 +0800

    simplify transFormat algo

--- Method/trans_format.py
+++ Method/trans_format.py
@@ -6,18 +6,20 @@ import open3d as o3d
 
 def transFormat(source_pointcloud_file_path,
                 target_pointcloud_file_path,
-                estimate_normals_radius,
-                estimate_normals_max_nn):
+                need_estimate_normals=False,
+                estimate_normals_radius=0.05,
+                estimate_normals_max_nn=30):
     if not os.path.exists(source_pointcloud_file_path):
-        print("[ERROR][transFormat]")
+        print("[ERROR][trans::transFormat]")
         print("source_file not exist!")
         return False
 
     pointcloud = o3d.io.read_point_cloud(source_pointcloud_file_path, print_progress=True)
-    pointcloud.estimate_normals(
-        search_param=o3d.geometry.KDTreeSearchParamHybrid(
-            radius=estimate_normals_radius,
-            max_nn=estimate_normals_max_nn))
+    if need_estimate_normals:
+        pointcloud.estimate_normals(
+            search_param=o3d.geometry.KDTreeSearchParamHybrid(
+                radius=estimate_normals_radius,
+                max_nn=estimate_normals_max_nn))
 
     o3d.io.write_point_cloud(
         target_pointcloud_file_path,
@@ -25,26 +27,4 @@ def transFormat(source_pointcloud_file_path,
         write_ascii=True,
         print_progress=True)
     return True
-
-def transToPCD(source_pointcloud_file_path,
-               estimate_normals_radius,
-               estimate_normals_max_nn):
-    if ".pcd" == source_pointcloud_file_path[-4:]:
-        return True
-    target_pointcloud_file_path = source_pointcloud_file_path[:-4] + ".pcd"
-    return transFormat(source_pointcloud_file_path,
-                       target_pointcloud_file_path,
-                       estimate_normals_radius,
-                       estimate_normals_max_nn)
-
-def transToPLY(source_pointcloud_file_path,
-               estimate_normals_radius,
-               estimate_normals_max_nn):
-    if ".ply" == source_pointcloud_file_path[-4:]:
-        return True
-    target_pointcloud_file_path = source_pointcloud_file_path[:-4] + ".ply"
-    return transFormat(source_pointcloud_file_path,
-                       target_pointcloud_file_path,
-                       estimate_normals_radius,
-                       estimate_normals_max_nn)
 
