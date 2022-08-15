c66c9037c0b40789b156b403f6dc7b3c0886b8e8
chLi <565353780@qq.com>
2022-08-15 17:45:44 +0800

    finish construct face_set from existed data

--- Data/face.py
+++ Data/face.py
@@ -17,3 +17,32 @@ class Face(object):
                 return False
         return True
 
+    def getMappingPointIdxList(self, mapping_dict):
+        mapping_point_idx_list = []
+        for point_idx in self.point_idx_list:
+            if str(point_idx) not in mapping_dict.keys():
+                print("[ERROR][Face::getMappingFace]")
+                print("\t mapping_dict not valid for this face!")
+                return None
+
+            mapping_point_idx = mapping_dict[str(point_idx)]
+            mapping_point_idx_list.append(mapping_point_idx)
+
+        return mapping_point_idx_list
+
+    def getMappingFace(self, mapping_dict):
+        mapping_point_idx_list = self.getMappingPointIdxList(mapping_dict)
+        if mapping_point_idx_list is None:
+            print("[ERROR][Face::getMappingFace]")
+            print("\t getMappingPointIdxList failed!")
+            return None
+
+        mapping_face = Face(mapping_point_idx_list)
+        return mapping_face
+
+    def outputInfo(self, info_level=0):
+        line_start = "\t" * info_level
+        print(line_start + "[Face]")
+        print(line_start + "\t point_idx_list =", self.point_idx_list)
+        return True
+
--- Data/face_set.py
+++ Data/face_set.py
@@ -39,3 +39,34 @@ class FaceSet(object):
         self.face_list.append(face)
         return True
 
+    def getFace(self, face_idx):
+        if face_idx >= len(self.face_list):
+            print("[ERROR][FaceSet::getFace]")
+            print("\t face_idx out of range!")
+            return None
+        return self.face_list[face_idx]
+
+    def getMappingFaceSet(self, face_idx_list, mapping_dict):
+        mapping_face_set = FaceSet()
+        for face_idx in face_idx_list:
+            face = self.getFace(face_idx)
+            if face is None:
+                print("[ERROR][FaceSet::getMappingFaceSet]")
+                print("\t getFace failed!")
+                return None
+
+            mapping_point_idx_list = face.getMappingPointIdxList(mapping_dict)
+            if mapping_point_idx_list is None:
+                print("[ERROR][FaceSet::getMappingFaceSet]")
+                print("\t getMappingPointIdxList failed!")
+                return None
+
+            mapping_face_set.addFace(mapping_point_idx_list)
+        return mapping_face_set
+
+    def outputInfo(self, info_level=0):
+        line_start = "\t" * info_level
+        print(line_start + "[FaceSet]")
+        print(line_start + "\t face_num =", len(self.face_list))
+        return True
+
