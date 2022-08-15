47219c11a75163c01b841642697819c15e3c363f
chLi <565353780@qq.com>
2022-08-15 12:54:23 +0800

    simplify loader for pointcloud

--- Module/pointcloud_loader.py
+++ Module/pointcloud_loader.py
@@ -5,7 +5,7 @@ from tqdm import tqdm
 
 from Data.channel_pointcloud import ChannelPointCloud
 
-from Method.path import getValidFilePath
+from Method.io import loadFileData
 
 class PointCloudLoader(object):
     def __init__(self):
@@ -16,97 +16,6 @@ class PointCloudLoader(object):
         self.channel_pointcloud.reset()
         return True
 
-    def loadPLY(self, ply_file_path):
-        valid_file_path = getValidFilePath(ply_file_path)
-        if valid_file_path is None:
-            print("[ERROR][loadPLY]")
-            print("\t getValidFilePath failed!")
-            return False
-
-        channel_name_list = []
-        lines = []
-        with open(valid_file_path, "r") as f:
-            lines = f.readlines()
-
-        point_num = -1
-        loaded_point_num = 0
-        find_start_line = False
-        for line in tqdm(lines):
-            if "property" in line:
-                if "property list" in line:
-                    continue
-                channel_name = line.split("\n")[0].split(" ")[2]
-                channel_name_list.append(channel_name)
-                continue
-
-            if "element vertex" in line:
-                point_num = int(line.split("\n")[0].split(" ")[2])
-                continue
-
-            if not find_start_line and "end_header" in line:
-                find_start_line = True
-                continue
-
-            if not find_start_line:
-                continue
-
-            line_data = line.split("\n")[0].split(" ")[:len(channel_name_list)]
-
-            channel_value_list = []
-            for i in range(len(channel_name_list)):
-                channel_value_list.append(float(line_data[i]))
-
-            self.channel_pointcloud.addChannelPoint(channel_name_list, channel_value_list)
-            loaded_point_num += 1
-
-            if loaded_point_num == point_num:
-                break
-        return True
-
-    def loadPCD(self, pcd_file_path):
-        valid_file_path = getValidFilePath(pcd_file_path)
-        if valid_file_path is None:
-            print("[ERROR][loadPCD]")
-            print("\t getValidFilePath failed!")
-            return False
-
-        channel_name_list = []
-        lines = []
-        with open(valid_file_path, "r") as f:
-            lines = f.readlines()
-
-        point_num = -1
-        loaded_point_num = 0
-        find_start_line = False
-        for line in tqdm(lines):
-            if "FIELDS" in line:
-                channel_name_list = line.split("\n")[0].split("FIELDS ")[1].split(" ")
-                continue
-
-            if "POINTS" in line:
-                point_num = int(line.split("\n")[0].split(" ")[1])
-                continue
-
-            if not find_start_line and "DATA ascii" in line:
-                find_start_line = True
-                continue
-
-            if not find_start_line:
-                continue
-
-            line_data = line.split("\n")[0].split(" ")[:len(channel_name_list)]
-
-            channel_value_list = []
-            for i in range(len(channel_name_list)):
-                channel_value_list.append(float(line_data[i]))
-
-            self.channel_pointcloud.addChannelPoint(channel_name_list, channel_value_list)
-            loaded_point_num += 1
-
-            if loaded_point_num == point_num:
-                break
-        return True
-
     def loadData(self, pointcloud_file_path):
         self.reset()
 
@@ -114,31 +23,22 @@ class PointCloudLoader(object):
         print("\t start load pointcloud :")
         print("\t pointcloud_file_path =", pointcloud_file_path)
 
-        if pointcloud_file_path[-4:] == ".pcd":
-            if not self.loadPCD(pointcloud_file_path):
-                print("[ERROR][PointCloudLoader::loadData]")
-                print("\t loadPCD failed!")
-                return False
+        channel_name_list, channel_value_list_list = loadFileData(pointcloud_file_path, True)
 
-            self.channel_pointcloud.updateKDTree()
-            return True
+        if channel_name_list == [] or channel_value_list_list == []:
+            print("[ERROR][PointCloudLoader::loadData]")
+            print("\t loadFileData failed!")
+            return False
 
-        if pointcloud_file_path[-4:] == ".ply":
-            if not self.loadPLY(pointcloud_file_path):
-                print("[ERROR][PointCloudLoader::loadData]")
-                print("\t loadPLY failed!")
-                return False
+        for channel_value_list in tqdm(channel_value_list_list):
+            self.channel_pointcloud.addChannelPoint(channel_name_list, channel_value_list)
 
-            self.channel_pointcloud.updateKDTree()
-            return True
-
-        print("[ERROR][PointCloudLoader::loadData]")
-        print("\t pointcloud_file type not valid!")
-        return False
+        self.channel_pointcloud.updateKDTree()
+        return True
 
 def demo():
     pointcloud_file_path = \
-        "/home/chli/chLi/OBJs/OpenGL/bunny_1.pcd"
+        "/home/chli/chLi/OBJs/OpenGL/bunny_1.ply"
 
     pointcloud_loader = PointCloudLoader()
     pointcloud_loader.loadData(pointcloud_file_path)
