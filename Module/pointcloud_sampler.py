dde8d0c4bd08873793d819bd427bbfa5875fd1cd
chLi <565353780@qq.com>
2022-08-16 18:42:37 +0800

    add basic sampler class struct

---
+++ Module/pointcloud_sampler.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class PointCloudSampler(object):
+    def __init__(self):
+        return
+
+def demo():
+    pointcloud_sampler = PointCloudSampler()
+    return True
+
