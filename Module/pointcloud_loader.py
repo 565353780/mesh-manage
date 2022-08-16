02f716c59c4fc7bfe3d9069b207938e2e107e3d7
chLi <565353780@qq.com>
2022-08-16 18:41:57 +0800

    simplify pointcloud load func

--- Module/pointcloud_loader.py
+++ Module/pointcloud_loader.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from tqdm import tqdm
-
 from Data.channel_pointcloud import ChannelPointCloud
-
-from Method.io import loadFileData
 
 class PointCloudLoader(object):
     def __init__(self):
@@ -17,23 +13,9 @@ class PointCloudLoader(object):
         return True
 
     def loadData(self, pointcloud_file_path):
-        self.reset()
-
-        print("[INFO][PointCloudLoader::loadData]")
-        print("\t start load pointcloud :")
-        print("\t pointcloud_file_path =", pointcloud_file_path)
-
-        channel_name_list, channel_value_list_list = loadFileData(pointcloud_file_path, True)
-
-        if channel_name_list == [] or channel_value_list_list == []:
+        if not self.channel_pointcloud.loadData(pointcloud_file_path):
             print("[ERROR][PointCloudLoader::loadData]")
-            print("\t loadFileData failed!")
-            return False
-
-        for channel_value_list in tqdm(channel_value_list_list):
-            self.channel_pointcloud.addChannelPoint(channel_name_list, channel_value_list)
-
-        self.channel_pointcloud.updateKDTree()
+            print("\t loadData failed!")
         return True
 
 def demo():
