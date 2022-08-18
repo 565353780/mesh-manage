cb3f8de75b07d7499f00f2f9d890407355bdac9f
chLi <565353780@qq.com>
2022-08-18 14:31:02 +0800

    add 2 more demo functions

--- demo.py
+++ demo.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from Module.pointcloud_painter import demo_merge, demo_paint, demo_auto_paint
+from Module.sampler import demo_sample_pointcloud, demo_sample_mesh
 from Module.mesh_loader import demo
 
 if __name__ == "__main__":
