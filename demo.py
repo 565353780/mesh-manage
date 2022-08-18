8acc3f6bd49c2261afb334545c2148f986b9e758
chLi <565353780@qq.com>
2022-08-18 17:39:06 +0800

    simplify demo code

--- demo.py
+++ demo.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from Module.pointcloud_painter import demo_merge, demo_paint, demo_auto_paint
+from Module.painter import demo_merge, demo_paint, demo_auto_paint
 from Module.sampler import demo_sample_pointcloud, demo_sample_mesh
-from Module.channel_pointcloud import demo as pointcloud_demo
-from Module.channel_mesh import demo as mesh_demo
+from Module.channel_mesh import demo
 
 if __name__ == "__main__":
-    mesh_demo()
+    demo()
 
