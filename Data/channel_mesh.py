67509aa6f8d3adb3dad7f41a694a22475cc5d9d7
chLi <565353780@qq.com>
2022-08-18 16:33:36 +0800

    remove mesh_loader, move all algo into channel_mesh class

--- Data/channel_mesh.py
+++ Data/channel_mesh.py
@@ -1,20 +1,48 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from tqdm import tqdm
+
 from Data.channel_pointcloud import ChannelPointCloud
 from Data.face_set import FaceSet
 
-from Method.io import saveChannelMesh
+from Method.io import loadFileData, saveChannelMesh
 
 class ChannelMesh(object):
-    def __init__(self, channel_pointcloud=ChannelPointCloud(), face_set=FaceSet()):
+    def __init__(self, mesh_file_path=None, channel_pointcloud=ChannelPointCloud(), face_set=FaceSet()):
         self.channel_pointcloud = channel_pointcloud
         self.face_set = face_set
+
+        if mesh_file_path is not None:
+            self.loadData(mesh_file_path)
         return
 
     def reset(self):
         self.channel_pointcloud.reset()
         self.face_set.reset()
+        return True
+
+    def loadData(self, mesh_file_path):
+        self.reset()
+
+        print("[INFO][ChannelMesh::loadData]")
+        print("\t start load mesh :")
+        print("\t mesh_file_path =", mesh_file_path)
+
+        channel_name_list, channel_value_list_list, point_idx_list_list = loadFileData(mesh_file_path)
+        if channel_name_list == [] or channel_value_list_list == []:
+            print("[ERROR][ChannelMesh::loadData]")
+            print("\t loadFileData failed!")
+            return False
+
+        for channel_value_list in tqdm(channel_value_list_list):
+            self.addChannelPoint(channel_name_list, channel_value_list)
+
+        self.updateKDTree()
+
+        if point_idx_list_list != []:
+            for point_idx_list in point_idx_list_list:
+                self.addFace(point_idx_list)
         return True
 
     def addChannelPoint(self, channel_name_list, channel_value_list):
@@ -57,7 +85,7 @@ class ChannelMesh(object):
             print("\t getMappingFaceSet failed!")
             return None
 
-        channel_mesh = ChannelMesh(channel_pointcloud, face_set)
+        channel_mesh = ChannelMesh(None, channel_pointcloud, face_set)
         return channel_mesh
 
     def getChannelMeshByPoint(self, point_idx_list):
@@ -73,13 +101,37 @@ class ChannelMesh(object):
             print("\t getFaceSetInPointIdxList failed!")
             return None
 
-        channel_mesh = ChannelMesh(channel_pointcloud, face_set)
+        channel_mesh = ChannelMesh(None, channel_pointcloud, face_set)
         return channel_mesh
 
     def saveMesh(self, save_file_path, print_progress=False):
         if not saveChannelMesh(self, save_file_path, print_progress):
             print("[ERROR][ChannelMesh::saveMesh]")
             print("\t saveChannelMesh failed!")
+            return False
+        return True
+
+    def generateMeshByFace(self, face_idx_list, save_file_path, print_progress=False):
+        channel_mesh = self.getChannelMeshByFace(face_idx_list)
+        if channel_mesh is None:
+            print("[ERROR][ChannelMesh::generateMeshByFace]")
+            print("\t getChannelMeshByFace failed!")
+            return False
+        if not channel_mesh.saveMesh(save_file_path, print_progress):
+            print("[ERROR][ChannelMesh::generateMeshByFace]")
+            print("\t saveMesh failed!")
+            return False
+        return True
+
+    def generateMeshByPoint(self, point_idx_list, save_file_path, print_progress=False):
+        channel_mesh = self.getChannelMeshByPoint(point_idx_list)
+        if channel_mesh is None:
+            print("[ERROR][ChannelMesh::generateMeshByPoint]")
+            print("\t getChannelMeshByPoint failed!")
+            return False
+        if not channel_mesh.saveMesh(save_file_path, print_progress):
+            print("[ERROR][ChannelMesh::generateMeshByPoint]")
+            print("\t saveMesh failed!")
             return False
         return True
 
--- Module/mesh_loader.py
+++ Module/mesh_loader.py
@@ -1,81 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from tqdm import tqdm
 
 from Data.channel_mesh import ChannelMesh
-
-from Method.io import loadFileData
-
-class MeshLoader(object):
-    def __init__(self, mesh_file_path=None):
-        self.channel_mesh = ChannelMesh()
-
-        if mesh_file_path is not None:
-            self.loadData(mesh_file_path)
-        return
-
-    def reset(self):
-        self.channel_mesh.reset()
-        return True
-
-    def loadData(self, mesh_file_path):
-        self.reset()
-
-        print("[INFO][MeshLoader::loadData]")
-        print("\t start load mesh :")
-        print("\t mesh_file_path =", mesh_file_path)
-
-        channel_name_list, channel_value_list_list, point_idx_list_list = loadFileData(mesh_file_path)
-        if channel_name_list == [] or channel_value_list_list == []:
-            print("[ERROR][MeshLoader::loadData]")
-            print("\t loadFileData failed!")
-            return False
-
-        for channel_value_list in tqdm(channel_value_list_list):
-            self.channel_mesh.addChannelPoint(channel_name_list, channel_value_list)
-
-        self.channel_mesh.updateKDTree()
-
-        if point_idx_list_list != []:
-            for point_idx_list in point_idx_list_list:
-                self.channel_mesh.addFace(point_idx_list)
-        return True
-
-    def generateMeshByFace(self, face_idx_list, save_file_path):
-        channel_mesh = self.channel_mesh.getChannelMeshByFace(face_idx_list)
-        if channel_mesh is None:
-            print("[ERROR][MeshLoader::generateMeshByFace]")
-            print("\t getChannelMeshByFace failed!")
-            return False
-        if not channel_mesh.saveMesh(save_file_path):
-            print("[ERROR][MeshLoader::generateMeshByFace]")
-            print("\t saveMesh failed!")
-            return False
-        return True
-
-    def generateMeshByPoint(self, point_idx_list, save_file_path):
-        channel_mesh = self.channel_mesh.getChannelMeshByPoint(point_idx_list)
-        if channel_mesh is None:
-            print("[ERROR][MeshLoader::generateMeshByPoint]")
-            print("\t getChannelMeshByPoint failed!")
-            return False
-        if not channel_mesh.saveMesh(save_file_path):
-            print("[ERROR][MeshLoader::generateMeshByPoint]")
-            print("\t saveMesh failed!")
-            return False
-        return True
 
 def demo():
     mesh_file_path = "/home/chli/chLi/ScanNet/scans/scene0474_02/scene0474_02_vh_clean_2.ply"
 
-    mesh_loader = MeshLoader(mesh_file_path)
+    channel_mesh = ChannelMesh(mesh_file_path)
 
     face_idx_list = [i for i in range(20)]
 
-    point_idx_list = mesh_loader.channel_mesh.getPointIdxListFromFaceIdxList(face_idx_list)
+    point_idx_list = channel_mesh.getPointIdxListFromFaceIdxList(face_idx_list)
 
-    mesh_loader.generateMeshByFace(face_idx_list, "/home/chli/chLi/channel_mesh/test1.ply")
-    mesh_loader.generateMeshByPoint(point_idx_list, "/home/chli/chLi/channel_mesh/test2.ply")
+    channel_mesh.generateMeshByFace(face_idx_list, "/home/chli/chLi/channel_mesh/test1.ply")
+    channel_mesh.generateMeshByPoint(point_idx_list, "/home/chli/chLi/channel_mesh/test2.ply")
     return True
 
