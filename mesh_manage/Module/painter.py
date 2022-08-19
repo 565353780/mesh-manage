692219c4937407535bf5749d48eff4b29468748f
chLi <565353780@qq.com>
2022-08-19 16:01:56 +0800

    finish merge multi meshes with different color

--- mesh_manage/Module/painter.py
+++ mesh_manage/Module/painter.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 import os
+from tqdm import tqdm
+from random import randint
 
 from mesh_manage.Config.color import d3_40_colors_rgb
 
@@ -33,12 +35,56 @@ class Painter(object):
         self.channel_mesh_dict[mesh_name] = channel_mesh
         return True
 
-    def loadMeshFilePathDict(self, mesh_file_path_dict):
-        for mesh_name, mesh_file_path in mesh_file_path_dict:
-            if not self.addChannelMesh(mesh_file_path, mesh_name):
-                print("[ERROR][Painter::loadMeshFilePathDict]")
-                print("\t addChannelMesh failed!")
-                return False
+    def loadMeshFilePathDict(self, mesh_file_path_dict, print_progress=False):
+        if print_progress:
+            print("[INFO][Painter::loadMeshFilePathDict]")
+            print("\t start load mesh files...")
+            for mesh_name, mesh_file_path in tqdm(mesh_file_path_dict.items()):
+                if not self.addChannelMesh(mesh_file_path, mesh_name):
+                    print("[ERROR][Painter::loadMeshFilePathDict]")
+                    print("\t addChannelMesh failed!")
+                    return False
+        else:
+            for mesh_name, mesh_file_path in mesh_file_path_dict.items():
+                if not self.addChannelMesh(mesh_file_path, mesh_name):
+                    print("[ERROR][Painter::loadMeshFilePathDict]")
+                    print("\t addChannelMesh failed!")
+                    return False
+        return True
+
+    def loadMeshFolder(self, mesh_folder_path, print_progress=False):
+        if mesh_folder_path[-1] != "/":
+            mesh_folder_path += "/"
+
+        if not os.path.exists(mesh_folder_path):
+            print("[ERROR][Painter::loadMeshFolder]")
+            print("\t mesh_folder not exist!")
+            return False
+
+        mesh_file_name_list = os.listdir(mesh_folder_path)
+
+        if print_progress:
+            print("[INFO][Painter::loadMeshFolder]")
+            print("\t start load mesh folder...")
+            for mesh_file_name in tqdm(mesh_file_name_list):
+                mesh_file_format = mesh_file_name.split(".")[-1]
+                mesh_file_basename = mesh_file_name.split("." + mesh_file_format)[0].replace(".", "_")
+                if mesh_file_name.split(".")[-1] not in ["obj", "ply", "pcd"]:
+                    continue
+                if not self.addChannelMesh(mesh_folder_path + mesh_file_name, mesh_file_basename):
+                    print("[ERROR][Painter::loadMeshFolder]")
+                    print("\t addChannelMesh failed!")
+                    return False
+        else:
+            for mesh_file_name in mesh_file_name_list:
+                mesh_file_format = mesh_file_name.split(".")[-1]
+                mesh_file_basename = mesh_file_name.split("." + mesh_file_format)[0].replace(".", "_")
+                if mesh_file_name.split(".")[-1] not in ["obj", "ply", "pcd"]:
+                    continue
+                if not self.addChannelMesh(mesh_folder_path + mesh_file_name, mesh_file_basename):
+                    print("[ERROR][Painter::loadMeshFolder]")
+                    print("\t addChannelMesh failed!")
+                    return False
         return True
 
     def mergeChannelMesh(self, merge_mesh_dict):
@@ -73,6 +119,38 @@ class Painter(object):
 
     def paintColor(self, paint_channel_name, color_list):
         self.merge_channel_mesh.paintByLabel(paint_channel_name, color_list)
+        return True
+
+    def mergeWithColor(self, print_progress=False):
+        self.merge_channel_mesh = ChannelMesh()
+
+        if len(self.channel_mesh_dict.keys()) == 0:
+            print("[WARN][Painter::mergeWithColor]")
+            print("\t channel_mesh_dict is empty!")
+            return True
+
+        if print_progress:
+            for _, channel_mesh in tqdm(self.channel_mesh_dict.items()):
+                color = [randint(0, 255), randint(0, 255), randint(0, 255)]
+
+                channel_name_list = ["x", "y", "z"]
+                channel_value_list_list = channel_mesh.getChannelValueListList(channel_name_list)
+                channel_name_list.extend(["r", "g", "b"])
+                for channel_value_list in channel_value_list_list:
+                    channel_value_list.extend(color)
+
+                self.merge_channel_mesh.addChannelPointList(channel_name_list, channel_value_list_list)
+        else:
+            for _, channel_mesh in self.channel_mesh_dict.items():
+                color = [randint(0, 255), randint(0, 255), randint(0, 255)]
+
+                channel_name_list = ["x", "y", "z"]
+                channel_value_list_list = channel_mesh.getChannelValueListList(channel_name_list)
+                channel_name_list.extend(["r", "g", "b"])
+                for channel_value_list in channel_value_list_list:
+                    channel_value_list.extend(color)
+
+                self.merge_channel_mesh.addChannelPointList(channel_name_list, channel_value_list_list)
         return True
 
     def savePaintedPointCloud(self, save_file_path, print_progress=False):
@@ -146,5 +224,15 @@ def demo_auto_paint():
     painter.removeOutlierPoints(outlier_dist_max)
     painter.paintColor(paint_channel_name, d3_40_colors_rgb)
     painter.savePaintedPointCloud(save_file_path)
+    return True
+
+def demo_merge_with_color():
+    mesh_folder_path = "/home/chli/chLi/ScanNet/objects/scene0000_00/"
+    save_file_path = "/home/chli/chLi/test_merge.obj"
+
+    painter = Painter()
+    painter.loadMeshFolder(mesh_folder_path, True)
+    painter.mergeWithColor(True)
+    painter.savePaintedMesh(save_file_path, True)
     return True
 
