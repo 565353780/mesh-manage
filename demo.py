3a92badeb6cc563da4243c3e20ca642eb369d308
chLi <565353780@qq.com>
2022-08-19 16:02:06 +0800

    add a new demo func, test passed

--- demo.py
+++ demo.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from mesh_manage.Module.painter import demo_merge, demo_paint, demo_auto_paint
+from mesh_manage.Module.painter import demo_merge, demo_paint, demo_auto_paint, demo_merge_with_color
 from mesh_manage.Module.sampler import demo_sample_pointcloud, demo_sample_mesh
 from mesh_manage.Module.channel_mesh import demo
 
 if __name__ == "__main__":
-    demo()
+    #  demo()
+    demo_merge_with_color()
 
