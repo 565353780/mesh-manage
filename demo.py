94eb91f2c06c237f8d9df9a8cd8604445b78f920
chLi <565353780@qq.com>
2022-08-19 16:21:57 +0800

    update demo func name

--- demo.py
+++ demo.py
@@ -3,9 +3,9 @@
 
 from mesh_manage.Module.painter import demo_merge, demo_paint, demo_auto_paint, demo_merge_with_color
 from mesh_manage.Module.sampler import demo_sample_pointcloud, demo_sample_mesh
-from mesh_manage.Module.channel_mesh import demo
+from mesh_manage.Module.channel_mesh import demo as demo_generate_mesh
 
 if __name__ == "__main__":
-    #  demo()
+    #  demo_generate_mesh()
     demo_merge_with_color()
 
