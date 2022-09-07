3047c30c783c88b554f07ae333d90222549c60ac
chLi <565353780@qq.com>
2022-09-07 20:09:36 +0800

    update trans params

--- trans.py
+++ trans.py
@@ -5,9 +5,9 @@ from mesh_manage.Method.trans import transFormat
 
 # Param
 source_pointcloud_file_path = \
-    "/home/chli/chLi/coscan_data/scene_result/matterport3d_01/coscan/2022_9_7_15-16-21_mp3d01_coscan/scene_29.pcd"
+    "/home/chli/chLi/coscan_data/scene_result/matterport3d_05/coscan/2022_9_7_19-3-4_mp3d05_coscan/scene_33.pcd"
 target_pointcloud_file_path = \
-    "/home/chli/chLi/coscan_data/scene_result/matterport3d_01/coscan/scene_29.ply"
+    "/home/chli/chLi/coscan_data/scene_result/matterport3d_05/coscan/scene_33.ply"
 
 # Process
 transFormat(source_pointcloud_file_path,
