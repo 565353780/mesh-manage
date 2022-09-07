c88316d2d841c4f822c30f655f16d60095299220
chLi <565353780@qq.com>
2022-09-07 18:56:21 +0800

    add move dict

---
+++ mesh_manage/Config/move.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+MOVE_LIST_DICT = {
+    "front3d_19": [2.32954, -3.56801, 0.0],
+    "matterport3d_01": [0.688111, -0.410494, 0.0],
+    "matterport3d_03": [-0.234072, 0.926202, 0.0],
+}
+
