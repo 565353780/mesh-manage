ceeaebf167c404b4c282c9745fdd62d72025868d
chLi <565353780@qq.com>
2022-09-07 18:56:40 +0800

    add trans_init move list

--- draw_heatmap.py
+++ draw_heatmap.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from mesh_manage.Config.move import MOVE_LIST_DICT
+
 from mesh_manage.Method.heatmap import getHeatMap
 
 def demo():
@@ -12,14 +14,19 @@ def demo():
         "/home/chli/chLi/coscan_data/scene_result/matterport3d_03/part_coscan.ply"
     save_complete_mesh_file_path = \
         "/home/chli/chLi/coscan_data/scene_result/matterport3d_03/comp_coscan.ply"
+    move_list = MOVE_LIST_DICT["matterport3d_03"]
     error_max = 0.5
+    is_visual = False
+    print_progress = True
 
     getHeatMap(partial_mesh_file_path,
                complete_mesh_file_path,
                save_partial_mesh_file_path,
                save_complete_mesh_file_path,
+               move_list=move_list,
                error_max=error_max,
-               print_progress=True)
+               is_visual=is_visual,
+               print_progress=print_progress)
     return True
 
 def demo_coscan():
@@ -28,9 +35,12 @@ def demo_coscan():
     gt_mesh_file_name = "19_cut.ply"
     coscan_result_file_name = "scene_29.ply"
     dong_result_file_name = "scene_27.ply"
+    move_list = MOVE_LIST_DICT["front3d_19"]
 
-    error_max = 0.1 # front3d
-    #  error_max = 0.5 # mp3d
+    #  error_max = 0.1 # front3d
+    error_max = 0.7 # mp3d
+    is_visual = False
+    print_progress = True
 
     coscan_partial_mesh_file_path = scene_result_folder_path + \
         scene_name + "/coscan/" + coscan_result_file_name
@@ -50,21 +60,29 @@ def demo_coscan():
     dong_save_complete_mesh_file_path = scene_result_folder_path + \
         scene_name + "/comp_dong.ply"
 
-    print("start get coscan heatmap...")
+    print("==================================")
+    print("==== start get coscan heatmap ====")
+    print("==================================")
     getHeatMap(coscan_partial_mesh_file_path,
                complete_mesh_file_path,
                coscan_save_partial_mesh_file_path,
                coscan_save_complete_mesh_file_path,
+               move_list=move_list,
                error_max=error_max,
-               print_progress=True)
+               is_visual=is_visual,
+               print_progress=print_progress)
 
-    print("start get dong heatmap...")
+    print("================================")
+    print("==== start get dong heatmap ====")
+    print("================================")
     getHeatMap(dong_partial_mesh_file_path,
                complete_mesh_file_path,
                dong_save_partial_mesh_file_path,
                dong_save_complete_mesh_file_path,
+               move_list=move_list,
                error_max=error_max,
-               print_progress=True)
+               is_visual=is_visual,
+               print_progress=print_progress)
     return True
 
 if __name__ == "__main__":
