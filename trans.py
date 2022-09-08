ca4f8dc90512976465cdbd0f947b416849f928f3
chLi <565353780@qq.com>
2022-09-08 13:00:13 +0800

    now will show progress as default

--- trans.py
+++ trans.py
@@ -5,12 +5,12 @@ from mesh_manage.Method.trans import transFormat
 
 # Param
 source_pointcloud_file_path = \
-    "/home/chli/chLi/coscan_data/scene_result/matterport3d_05/coscan/2022_9_7_19-3-4_mp3d05_coscan/scene_33.pcd"
+    "/home/chli/chLi/coscan_data/scene_result/matterport3d_05/dong/2022_9_8_12-29-59_mp3d05_dong/scene_16.pcd"
 target_pointcloud_file_path = \
-    "/home/chli/chLi/coscan_data/scene_result/matterport3d_05/coscan/scene_33.ply"
+    "/home/chli/chLi/coscan_data/scene_result/matterport3d_05/dong/scene_16.ply"
 
 # Process
 transFormat(source_pointcloud_file_path,
             target_pointcloud_file_path,
-            True)
+            True, True)
 
