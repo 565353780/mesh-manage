f2306830b71eb59a95d0bf4169aabe0d99e24178
chLi <565353780@qq.com>
2022-10-04 17:29:12 +0800

    add new render demo

--- demo.py
+++ demo.py
@@ -5,8 +5,10 @@ from mesh_manage.Module.painter import demo_merge, demo_paint, demo_auto_paint, 
 from mesh_manage.Module.sampler import demo_sample_pointcloud, demo_sample_mesh
 from mesh_manage.Module.channel_mesh import demo as demo_generate_mesh
 
+from mesh_manage.Demo.renderer import demo as demo_render
+
 if __name__ == "__main__":
     #  demo_generate_mesh()
     #  demo_merge_with_color()
-    demo_sample_mesh()
-
+    #  demo_sample_mesh()
+    demo_render()
