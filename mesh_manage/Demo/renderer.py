b326ce47b4b1c1d1bd9e9a8bd25b64320de10956
chLi <565353780@qq.com>
2022-10-04 18:03:24 +0800

    update renderer usage

--- mesh_manage/Demo/renderer.py
+++ mesh_manage/Demo/renderer.py
@@ -5,15 +5,16 @@ from mesh_manage.Module.renderer import Renderer
 
 
 def demo():
-    renderer = Renderer()
 
     for idx in range(28):
         mesh_file_path = \
                 "/home/chli/chLi/coscan_data/fast_forward/ff_recon_result_render_gray_blue/merged_vpp_mesh_" + str(idx) + ".ply"
         save_image_file_path = \
-                "/home/chli/chLi/coscan_data/fast_forward/ff_recon_result_render_gray_blue/merged_vpp_mesh_" + str(idx) + ".png"
+                "/home/chli/chLi/coscan_data/fast_forward/ff_recon_result_render_gray_blue_image/merged_vpp_mesh_" + str(idx) + ".png"
 
+        renderer = Renderer()
+
+        print("start render image :", idx + 1, "/", 28)
         #  renderer.renderMesh(mesh_file_path)
         renderer.saveRenderMeshImage(mesh_file_path, save_image_file_path)
-        return
     return True
