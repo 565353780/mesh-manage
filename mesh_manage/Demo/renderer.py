cbb979d8eb5a8b766013accb3586e2477e046fb3
chLi <565353780@qq.com>
2022-10-05 18:14:09 +0800

    add test for range

--- mesh_manage/Demo/renderer.py
+++ mesh_manage/Demo/renderer.py
@@ -5,8 +5,8 @@ from mesh_manage.Module.renderer import Renderer
 
 
 def demo():
-
     for idx in range(28):
+    #  for idx in range(27, -1, -1):
         mesh_file_path = \
                 "/home/chli/chLi/coscan_data/fast_forward/ff_recon_result_render_gray_blue/merged_vpp_mesh_" + str(idx) + ".ply"
         save_image_file_path = \
